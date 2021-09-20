58c501724fc0059649b45fb72ecd68f65c8a148a
Joseph Nutt <joenutt01@gmail.com>
2021-09-20 16:35:30 +0100

    Enable CORS to parse data from server to client

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,7 +40,17 @@ namespace API
             services.AddDbContext<DataContext>(options =>
             {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection")); 
-            }); 
+            });
+
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+                });
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,11 +64,12 @@ namespace API
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
 
-            app.UseAuthorization();
+            app.UseCors("CorsPolicy");
 
+
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
